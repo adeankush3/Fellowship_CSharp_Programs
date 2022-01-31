d86ded8c9ca68393bdc5f6ffd02ca145813c1153
adeankush3 <adeankush3@gmail.com>
2022-01-31 20:45:25 +0530

    [Ankush]Leap Year Program

--- Factor/Factor/Program.cs
+++ Factor/Factor/Program.cs
@@ -4,12 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Factor
+namespace Fellowship_CSharp_Programs
 {
-    internal class Program
+    public class Factors
     {
-        static void Main(string[] args)
+        int num;
+        public void Factor()
         {
+            Console.WriteLine("Enter a number : ");
+            num = Convert.ToInt32(Console.ReadLine());
+            int i, isPrime;
+
+            for (i = 2; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    isPrime = 1;
+                    for (int j = 2; j <= i / 2; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            isPrime = 0;
+                            return;
+                        }
+                    }
+
+                    if (isPrime == 1)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
         }
     }
 }
+
